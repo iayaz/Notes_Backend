9b6396bb3898fa39d1c1b9ffa92167ad29daab62
iayaz1 <iamlordayaz@gmail.com>
2024-01-03 23:48:56 +0530

    updated import statements to resolve the error

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
-const app = express();
 // Mounting the dpendencies
+const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
 
 //Routes
-
+app.use("/auth", authRoutes);
 // DB setup
 const PORT = process.env.PORT || 6001;
 mongoose
